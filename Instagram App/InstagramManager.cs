c05bdd50b0514b131fe537b7d876177445f38401
Luiz Felipe Escandiuzzi <escandiuzzilf@gmail.com>
2021-09-19 11:34:31 -0300

    Initial Commit

---
+++ Instagram App/InstagramManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstaSharper.API;
+using InstaSharper.API.Builder;
+using InstaSharper.Classes;
+using InstaSharper.Classes.Models;
+using InstaSharper.Logger;
+
+namespace Instagram_App
+{
+    public class InstagramManager
+    {
+        string username;
+        string password;
+
+        bool filterVerified;
+
+        private static UserSessionData user;
+        private static IInstaApi api;
+
+
+        public InstagramManager(string username, string password, bool filterVerified) 
+        {
+            this.username = username;
+            this.password = password;
+
+            this.filterVerified = filterVerified;
+
+            LoginAsync();
+        }
+
+
+        public async Task LoginAsync() 
+        {
+            user = new UserSessionData();
+
+            user.UserName = username;
+            user.Password = password;
+
+            api = InstaApiBuilder.CreateBuilder()
+                .SetUser(user)
+                .UseLogger(new DebugLogger(LogLevel.Exceptions))
+                .Build();
+
+            var loginRequest = await api.LoginAsync();
+
+            if (loginRequest.Succeeded)
+            {
+                Console.WriteLine("\n\nLogged in!\n\n");
+                GetAnalysisAsync();
+            }
+            else
+                Console.WriteLine("\n\nError Loggin in!\n" + loginRequest.Info.Message);
+        }
+
+        private async Task GetAnalysisAsync()
+        {
+            var following = await api.GetUserFollowingAsync(username, PaginationParameters.Empty);
+            var followers = await api.GetUserFollowersAsync(username, PaginationParameters.Empty);
+
+            List<InstaUserShort> filteredList = new List<InstaUserShort>();
+
+            if (filterVerified)
+                filteredList = following.Value.ToList().Where(x => !followers.Value.ToList().Contains(x) && !x.IsVerified).ToList();
+            else
+                filteredList = following.Value.ToList().Where(x => !followers.Value.ToList().Contains(x)).ToList();
+
+            foreach (var user in filteredList.OrderBy(x => x.UserName))
+                Console.WriteLine($"Username: {user.UserName}\nName: {user.FullName}\n");;
+        }
+    }
+}
---
+++ Instagram App/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Instagram_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------- InstaManager ---------------------------------\n\n");
+
+            string username = string.Empty;
+            string password = string.Empty;
+            string option = string.Empty;
+
+            Console.WriteLine("Insert your username?");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Insert your password?");
+
+            password = string.Empty;
+            ConsoleKey key;
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    password = password[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    password += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+
+
+            Console.WriteLine("\nDo you want to remove verified users? Y/N");
+            option = Console.ReadLine();
+
+            InstagramManager instaManager = new InstagramManager(username, password, option.ToUpper() == "Y");
+            Console.Read();
+        }
+    }
+}
